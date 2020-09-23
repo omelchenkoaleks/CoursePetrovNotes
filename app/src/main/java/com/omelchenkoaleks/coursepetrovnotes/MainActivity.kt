53f797f77582aef331934a36f7f495492c0d8a42
Omelchenko Aleksandr <omelchenkoaleks@gmail.com>
2020-09-23 07:32:51 +0300

    Инициализация объектов в MainActivity.

--- app/src/main/java/com/omelchenkoaleks/coursepetrovnotes/MainActivity.kt
+++ app/src/main/java/com/omelchenkoaleks/coursepetrovnotes/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.omelchenkoaleks.coursepetrovnotes
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.omelchenkoaleks.coursepetrovnotes.databinding.ActivityMainBinding
+import com.omelchenkoaleks.coursepetrovnotes.utilits.APP_ACTIVITY
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mToolbar: Toolbar
+    lateinit var mNavController: NavController
+    private var _binding: ActivityMainBinding? = null
+    val mBinding get() = _binding!! // Это для удобства, теперь не нужно будет проверять ссылку на null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+        APP_ACTIVITY = this
+        mToolbar = mBinding.toolbar
+        mNavController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        setSupportActionBar(mToolbar)
+        title = getString(R.string.title)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null // Это делается уже после того, как была удалена активити. Избегаем утечки памяти.
     }
 }
---
+++ app/src/main/java/com/omelchenkoaleks/coursepetrovnotes/utilits/constants.kt
@@ -0,0 +1,5 @@
+package com.omelchenkoaleks.coursepetrovnotes.utilits
+
+import com.omelchenkoaleks.coursepetrovnotes.MainActivity
+
+lateinit var APP_ACTIVITY: MainActivity
