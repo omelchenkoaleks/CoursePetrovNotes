f7254749d6a73b7d8a44b548540bf142372fb08c
Omelchenko Aleksandr <omelchenkoaleks@gmail.com>
2020-09-23 09:54:20 +0300

    Инициализация StartFragment и создание модели для заметки.

---
+++ app/src/main/java/com/omelchenkoaleks/coursepetrovnotes/screens/start/StartFragment.kt
@@ -0,0 +1,39 @@
+package com.omelchenkoaleks.coursepetrovnotes.screens.start
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.omelchenkoaleks.coursepetrovnotes.R
+import com.omelchenkoaleks.coursepetrovnotes.databinding.FragmentStartBinding
+import com.omelchenkoaleks.coursepetrovnotes.utilits.TYPE_ROOM
+import kotlinx.android.synthetic.main.fragment_start.*
+
+class StartFragment : Fragment() {
+
+    private var _binding: FragmentStartBinding? = null
+    private val mBinding get() = _binding!!
+    private lateinit var mViewModel: StartFragmentViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentStartBinding.inflate(layoutInflater, container, false)
+        return mBinding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initialization()
+    }
+
+    private fun initialization() {
+        mViewModel = ViewModelProvider(this).get(StartFragmentViewModel::class.java)
+        btn_room.setOnClickListener {
+            mViewModel.initDatabase(TYPE_ROOM)
+        }
+    }
+}
--- app/src/main/java/com/omelchenkoaleks/coursepetrovnotes/utilits/constants.kt
+++ app/src/main/java/com/omelchenkoaleks/coursepetrovnotes/utilits/constants.kt
@@ -3,3 +3,5 @@ package com.omelchenkoaleks.coursepetrovnotes.utilits
 import com.omelchenkoaleks.coursepetrovnotes.MainActivity
 
 lateinit var APP_ACTIVITY: MainActivity
+const val TYPE_DATABASE = "type_database"
+const val TYPE_ROOM = "type_room"
