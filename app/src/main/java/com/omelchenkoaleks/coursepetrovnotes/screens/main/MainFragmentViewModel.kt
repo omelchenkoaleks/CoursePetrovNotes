bbc21a79d33edc6f504019672975a9f5c54b4493
Omelchenko Aleksandr <omelchenkoaleks@gmail.com>
2020-09-23 12:49:40 +0300

    Реализация RecyclerView. Отображение заметок в списке.

--- app/src/main/java/com/omelchenkoaleks/coursepetrovnotes/screens/main/MainFragmentViewModel.kt
+++ app/src/main/java/com/omelchenkoaleks/coursepetrovnotes/screens/main/MainFragmentViewModel.kt
@@ -2,7 +2,8 @@ package com.omelchenkoaleks.coursepetrovnotes.screens.main
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import com.omelchenkoaleks.coursepetrovnotes.utilits.REPOSITORY
 
 class MainFragmentViewModel(application: Application): AndroidViewModel(application) {
-
+    val allNotes = REPOSITORY.allNotes
 }
